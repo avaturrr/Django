2134971a3e39663f6bc0d96ff51546bb23e8d1e2
Avaturrr <avaturrr@gmail.com>
2023-07-06 19:54:10 +0300

    django_09

---
+++ src/app2/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+from django.db import models
+
+
+# Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100, null=True, blank=True)
+    lastname = models.CharField(max_length=100, null=True, blank=True)
+    age = models.SmallIntegerField()
+    profession = models.CharField(max_length=100, null=True, blank=True)
+
